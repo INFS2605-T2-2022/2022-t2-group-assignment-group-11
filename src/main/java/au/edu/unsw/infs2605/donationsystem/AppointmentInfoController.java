59235d7552331ca0a24261f33cb98496b342e4b5
kellytrangdao <z5261905@student.unsw.edu.au>
2022-07-31 10:39:24 +1000

    update

---
+++ src/main/java/au/edu/unsw/infs2605/donationsystem/AppointmentInfoController.java
@@ -0,0 +1,179 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package au.edu.unsw.infs2605.donationsystem;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+
+import java.sql.SQLException;
+
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author w_yy
+ */
+public class AppointmentInfoController {
+    @FXML
+    private TableView<AppointmentInfo> AppointmentTable;
+
+    @FXML
+    private TableColumn<AppointmentInfo, String> ID;
+
+    @FXML
+    private TableColumn<AppointmentInfo, String> donationType;
+
+    @FXML
+    private TableColumn<AppointmentInfo, String> donorCentre;
+
+    @FXML
+    private TableColumn<AppointmentInfo, String> Date;
+
+    @FXML
+    private TableColumn<AppointmentInfo, String> Time;
+
+    @FXML
+    private TextField idField;
+
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private TextField donorCentreField;
+
+    @FXML
+    private TextField donationDateField;
+
+    @FXML
+    private TextField phoneNumberField;
+
+    @FXML
+    private TextField emailAddressField;
+
+    @FXML
+    private TextField donationTimeField;
+
+    @FXML
+    private TextField donationStatusField;
+
+    @FXML
+    private TextField donationTypeField;
+
+    @FXML
+    private TextField notesFiled;
+
+    @FXML
+    private TextField keywordSearch;
+    @FXML
+    private ComboBox<String> filterField;
+
+
+    public void initialize() {
+        ID.setCellValueFactory(new PropertyValueFactory<>("ID"));
+        donationType.setCellValueFactory(new PropertyValueFactory<>("donationType"));
+        donorCentre.setCellValueFactory(new PropertyValueFactory<>("donorCentre"));
+        Date.setCellValueFactory(new PropertyValueFactory<>("donationDate"));
+        Time.setCellValueFactory(new PropertyValueFactory<>("donationTime"));
+
+        keywordSearch.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.length() > 0) {
+                String selectedItem = filterField.getSelectionModel().getSelectedItem();
+               
+                AppointmentTable.setItems(Database.getInstance().getAppointmentInfo(" where "+selectedItem + " like '%"+newValue+"%'"));
+            } else {
+                AppointmentTable.setItems(Database.getInstance().getAppointmentInfo(""));
+            }
+        });
+
+       
+        filterField.getItems().addAll("firstName", "lastName", "donationType", "donorCentre", "emailAddress");
+        filterField.getSelectionModel().select(0);
+        Platform.runLater(() -> {
+            AppointmentTable.getScene().getWindow().focusedProperty()
+                    .addListener((observableValue, aBoolean, newValue) -> {
+                        if (newValue) {
+                            AppointmentTable.requestFocus();
+                            int selectedIndex = AppointmentTable.getSelectionModel().getSelectedIndex();
+                            AppointmentTable.getSelectionModel().clearSelection();
+                            AppointmentTable.getSelectionModel().select(selectedIndex);
+                            AppointmentTable.setItems(Database.getInstance().getAppointmentInfo(""));
+                        }
+                    });
+        });
+        AppointmentTable.getSelectionModel().selectedItemProperty().addListener((observableValue, appointmentInfo, newValue) -> {
+            if (newValue != null) {
+                ClickSidebar(newValue);
+            }
+        });
+
+        AppointmentTable.setItems(Database.getInstance().getAppointmentInfo(""));
+    }
+
+    @FXML
+    private void ClickSidebar(AppointmentInfo selected) {
+        idField.setText(selected.getID() + "");
+        firstNameField.setText(selected.getFirstName());
+        lastNameField.setText(selected.getLastName());
+        donorCentreField.setText(selected.getDonorCentre());
+        donationTimeField.setText(selected.getDonationTime());
+        donationDateField.setText(selected.getDonationDate());
+        phoneNumberField.setText(selected.getPhoneNumber());
+        emailAddressField.setText(selected.getEmailAddress());
+        donationStatusField.setText(selected.getDonationStatus());
+        donationTypeField.setText(selected.getDonationType());
+        notesFiled.setText(selected.getNotes());
+
+
+    }
+
+    @FXML
+    private void Add() {
+        showEdit(null, "Edit.fxml");
+    }
+
+    @FXML
+    private void Edit() {
+        AppointmentInfo selected = AppointmentTable.getSelectionModel().getSelectedItem();
+        if (selected == null) {
+            return;
+        }
+        showEdit(selected, "Edit.fxml");
+    }
+
+    @FXML
+    private void Delete() {
+        AppointmentInfo selected = AppointmentTable.getSelectionModel().getSelectedItem();
+        if (selected == null) {
+            return;
+        }
+        Database.getInstance().deleteAppointmentInfo(selected);
+        AppointmentTable.setItems(Database.getInstance().getAppointmentInfo(""));
+        new Alert(Alert.AlertType.INFORMATION, "success").showAndWait();
+
+    }
+
+    private void showEdit(AppointmentInfo appointmentInfo, String fxml) {
+        try {
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource(fxml));
+            Scene scene = new Scene(root);
+            scene.setUserData(appointmentInfo);
+            stage.setScene(scene);
+            stage.showAndWait();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
---
+++ src/main/java/au/edu/unsw/infs2605/donationsystem/EditController.java
@@ -0,0 +1,119 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package au.edu.unsw.infs2605.donationsystem;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author w_yy
+ */
+public class EditController {
+     @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private TextField idField;
+
+    @FXML
+    private TextField donorCentreField;
+
+    @FXML
+    private TextField donationTimeField;
+
+    @FXML
+    private TextField donationDateField;
+
+    @FXML
+    private TextField phoneNumberField;
+
+    @FXML
+    private TextField emailAddressField;
+
+    @FXML
+    private TextField notesField;
+
+    @FXML
+    private ComboBox<String> donationTypeField;
+
+    @FXML
+    private ComboBox<String> donationStatusField;
+    AppointmentInfo selected;
+
+    @FXML
+    public void initialize() {
+        donationTypeField.getItems().addAll("Blood", "Plasma", "Blood and Plasma");
+        donationStatusField.getItems().addAll("approved", "completed");
+        donationTypeField.getSelectionModel().select(0);
+        donationStatusField.getSelectionModel().select(0);
+        Platform.runLater(() -> {
+            Scene scene = idField.getScene();
+            if (scene.getUserData() != null) {
+                selected = (AppointmentInfo) scene.getUserData();
+                idField.setText(selected.getID() + "");
+                firstNameField.setText(selected.getFirstName());
+                lastNameField.setText(selected.getLastName());
+                donorCentreField.setText(selected.getDonorCentre());
+                donationTimeField.setText(selected.getDonationTime());
+                donationDateField.setText(selected.getDonationDate());
+                phoneNumberField.setText(selected.getPhoneNumber());
+                emailAddressField.setText(selected.getEmailAddress());
+                notesField.setText(selected.getNotes());
+                donationTypeField.getSelectionModel().select(selected.getDonationType());
+                donationStatusField.getSelectionModel().select(selected.getDonationStatus());
+            }
+        });
+    }
+
+    @FXML
+    private void save() {
+        String firstNameFieldText = firstNameField.getText();
+        String lastNameFieldText = lastNameField.getText();
+        String donorCentreFieldText = donorCentreField.getText();
+        String donationTimeFieldText = donationTimeField.getText();
+        String donationDateFieldText = donationDateField.getText();
+        String phoneNumberFieldText = phoneNumberField.getText();
+        String emailAddressFieldText = emailAddressField.getText();
+        String notesFieldText = notesField.getText();
+        String donationType = donationTypeField.getSelectionModel().getSelectedItem();
+        String donationStatus = donationStatusField.getSelectionModel().getSelectedItem();
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        try {
+            sdf.parse(donationDateFieldText);
+        } catch (ParseException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).showAndWait();
+            return;
+        }
+        if (selected == null) {
+            Database.getInstance().addAppointmentInfo(new AppointmentInfo(0, firstNameFieldText, lastNameFieldText, donorCentreFieldText,
+                    donationTimeFieldText, donationDateFieldText, emailAddressFieldText,
+                    phoneNumberFieldText, donationType, donationStatus, notesFieldText));
+        } else {
+            selected.setID(Integer.parseInt(idField.getText()));
+            selected.setFirstName(firstNameFieldText);
+            selected.setLastName(lastNameFieldText);
+            selected.setDonorCentre(donorCentreFieldText);
+            selected.setDonationTime(donationTimeFieldText);
+            selected.setPhoneNumber(phoneNumberFieldText);
+            selected.setEmailAddress(emailAddressFieldText);
+            selected.setDonationType(donationType);
+            selected.setDonationStatus(donationStatus);
+            Database.getInstance().UpdateAppointmentInfo(selected);
+        }
+        ((Stage) idField.getScene().getWindow()).close();
+    }
+        
+}
